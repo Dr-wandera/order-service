26cc36469f497a3dab1b2901e76c8562262ca5bd
Dr-wandera <wanderafaustine@gmail.com>
2026-05-24 22:15:23 +0300

    added repository method to find order by date

--- src/main/java/com/wanderaTech/order_service/Repository/OrderRepository.java
+++ src/main/java/com/wanderaTech/order_service/Repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.wanderaTech.order_service.Repository;
 
 import com.wanderaTech.order_service.Model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,6 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
     Optional<Order> findByOrderNumber(String orderNumber);
 
     List<Order> findAllByUserId(String userId);
+
+	Page<Order> findByOrderDate(LocalDate orderDate, Pageable pageable);
 }
